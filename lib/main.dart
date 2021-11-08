79c3b9c136049d7212f309c99d2f038fc2e9f99d
Gabriel Coimbra <gabao.coimbra@gmail.com>
2021-11-08 15:59:38 -0300

    Alterando Cor da Mensagem

--- lib/main.dart
+++ lib/main.dart
@@ -58,10 +58,10 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                isFull ? "Lotado" : "Pode entrar!",
+                isFull ? "Lotado!" : "Pode entrar!",
                 style: TextStyle(
                     fontSize: 30,
-                    color: Colors.white,
+                    color: isFull ? Colors.red : Colors.white,
                     fontWeight: FontWeight.w800),
               ),
               SizedBox(
@@ -71,7 +71,7 @@ class _HomePageState extends State<HomePage> {
                 '$count',
                 style: TextStyle(
                   fontSize: 100,
-                  color: Colors.white,
+                  color: isFull ? Colors.red : Colors.white,
                 ),
               ),
               SizedBox(
